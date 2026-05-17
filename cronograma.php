570b8ce815d4d6eb12f6bd2ff1b0c8af510eada2
Gustavinalb <gustazin.2501.albuquerque@gmail.com>
2026-05-17 19:49:19 -0300

    continuando

--- cronograma.php
+++ cronograma.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bar dali</title>
+    <title>Cronograma - Brasa Bar</title>
     <link rel="stylesheet" href="styles.css">
+
+    <!-- Icons -->
+    <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
-
-
 <body>
     <header>
         <div class="logo">
@@ -23,22 +24,91 @@
             </ul>
         </nav>
     </header>
-    <main>
+
+    <section class="cronograma">
         <div class="cabeçalio">
-            <h2>Cronograma De Eventos</h2>
+            <h2>Cronograma de Eventos</h2>
         </div>
-        <!-- Exemplo HTML -->
-        <div class="container-grid">
-        <div class="item">Segunda</div>
-        <div class="item">Terça</div>
-        <div class="item">Quarta</div>
-        <div class="item">Quinta</div>
-        <div class="item">Sexta</div>
-        <div class="item">Sabado</div>
-        <div class="item">Domingo</div>
-        </div>
+        <div class="grid-eventos">
+            <!-- Segunda -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Segunda</h2>
+                </div>
 
-    </main>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-calendar-xmark icone"></i>
+                    <h3>Sem eventos</h3>
+                </div>
+            </div>
+            <!-- Terça -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Terça</h2>
+                </div>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-microphone icone"></i>
+                    <h4>20:00</h4>
+                    <h3>Karaokê</h3>
+                </div>
+            </div>
+            <!-- Quarta -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Quarta</h2>
+                </div>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-beer-mug-empty icone"></i>
+                    <h4>18:00</h4>
+                    <h3>Happy Hour</h3>
+                </div>
+            </div>
+            <!-- Quinta -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Quinta</h2>
+                </div>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-guitar icone"></i>
+                    <h4>21:00</h4>
+                    <h3>Rock ao Vivo</h3>
+                </div>
+            </div>
+            <!-- Sexta -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Sexta</h2>
+                </div>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-headphones icone"></i>
+                    <h4>22:00</h4>
+                    <h3>DJ Night</h3> 
+                </div>
+            </div>
+            <!-- Sábado -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Sábado</h2>
+                </div>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-star icone"></i>
+                    <h4>21:00</h4>
+                    <h3>Sertanejo Universitário</h3>
+                </div>
+            </div>
+            <!-- Domingo -->
+            <div class="card">
+                <div class="topo-card">
+                    <h2>Domingo</h2>
+                </div>
+                <div class="conteudo-card">
+                    <i class="fa-solid fa-bottle-water icone"></i>
+                    <h4>17:00</h4>
+                    <h3>Domingueira Premium</h3>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
-
+<footer><h1>desenvolvido por gustavo</h1></footer>
 </html>
